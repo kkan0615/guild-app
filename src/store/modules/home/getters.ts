22c845d53124ad469e446a10fdad73fa1009c404
kkan0615 <kkan0615@gmail.com>
2021-09-03 22:25:36 +0900

    :hamer: Fix: fix errors for build

--- src/store/modules/home/getters.ts
+++ src/store/modules/home/getters.ts
@@ -1,10 +1,10 @@
 import { GetterTree } from 'vuex'
 import { RootState } from '@/store'
 import { HomeState } from '@/store/modules/home/state'
-import { GuildInfo } from '@/types/model/guilds'
+import { GuildInDropdown } from '@/types/model/guilds'
 
 export type HomeGetters<S = HomeState> = {
-  guildList(state: S): Array<GuildInfo>
+  guildList(state: S): Array<GuildInDropdown>
 }
 
 export const homeGetters: GetterTree<HomeState, RootState> & HomeGetters = {
